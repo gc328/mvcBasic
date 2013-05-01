7d528981b32178b62fd240cae4568996bdaeb6ae
Gabe Silvarajoo <gabrielchandran@yahoo.com>
2013-05-01 10:33:28 -0500

    New Files from MVC NHibernate Project

---
+++ MvcBasic/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Entity.Models;
+using DataAccess;
+
+
+namespace MvcBasic.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var productRepository = new Repository<Products>();
+
+            Products p = new Products { Name = "Nasi Goreng", Price = 2.5 };
+            productRepository.SaveOrUpdate(p);
+
+            return View(productRepository.GetAll());
+        }
+
+    }
+}
